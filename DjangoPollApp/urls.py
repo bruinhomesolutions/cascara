643b35abc30be3a334fda7625f4038ad08f34d90
Ben Yang <thisisbenbtw@gmail.com>
2018-01-02 20:59:55 -0800

    updated python and django

--- DjangoPollApp/urls.py
+++ DjangoPollApp/urls.py
@@ -7,37 +7,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 import django.contrib.auth.views
 
-import app.forms
-import app.views
-
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('app.urls', namespace="app")),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^seed', app.views.seed, name='seed'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    url(r'^', include('getdata.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^getdata/', include('getdata.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
--- getdata/views.py
+++ getdata/views.py
@@ -3,6 +3,7 @@ from .forms import DataForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+import django
 
 # Create your views here.
 def index(request):
@@ -20,5 +21,5 @@ def index(request):
     # if a GET (or any other method) we'll create a blank form
     else:
         form = DataForm()
-
-    return render(request, 'getdata/index.html', {'form': form})
+    vers = django.get_version()
+    return render(request, 'getdata/index.html', {'form': form,'version': vers})
